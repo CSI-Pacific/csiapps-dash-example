3dd73fb36713e36265e329605c0f0afe503cb687
rbrodiecsi <rbrodie@csipacific.ca>
2025-06-20 15:40:37 -0700

    profile api revisions

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import Dash, Input, Output, html, dcc, no_update, dash_table
 import requests
 import os
 import json
+import traceback
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,7 +18,7 @@ TOKEN_URL = f"{SITE_URL}/o/token/"
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-API_PEOPLE_URL = f"{SITE_URL}/api/registration/persons/"
+API_PEOPLE_URL = f"{SITE_URL}/api/registration/profile/"
 API_ME_URL = f"{SITE_URL}/api/csiauth/me/"
 
 # creating the instance of our auth class
@@ -34,6 +35,8 @@ server = (
 app = Dash(__name__, server=server)  # instantiating our app using this server
 
 app.layout = html.Div([
+    dcc.Location(id="redirect-to", refresh=True),
+
     dcc.Interval(
         id="init-interval",
         interval=500,  # e.g., 1 second after page load
@@ -42,14 +45,16 @@ app.layout = html.Div([
     ),
     html.H2("Who am I?"),
     html.Pre(id="current-user"),
-    html.H2("People List"),
+    html.H2("Profile List"),
     dash_table.DataTable(
         id="person-table",
         columns=[
             {"name": "ID", "id": "id"},
             {"name": "First Name", "id": "first_name"},
             {"name": "Last Name", "id": "last_name"},
+            {"name": "Sport", "id":"sport"},
             {"name": "Email", "id": "email"},
+            {"name": "Enrollment", "id":"enrollment_status"}
         ],
         style_table={"overflowX": "auto"},
         style_cell={"textAlign": "left"},
@@ -57,16 +62,29 @@ app.layout = html.Div([
 ])
 
 @app.callback(
+    Output("redirect-to", "href"),
     Output("person-table", "data"),
     Output("current-user", "children"),
     Input("init-interval", "n_intervals")
 )
 def initial_view(n):
     """When user clicks 'Fetch DRF Data', call the DRF endpoint with the OAuth token."""
-    token = auth.get_token()
+    try:
+        token = auth.get_token()
+    except Exception as e:
+
+        ## HERE, HOW DO I FORWARD A REDIRECT TO APP_URL???
+
+        print(e)
+
+        auth_url = f"{AUTH_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={APP_URL}"
+
+        print(f"Forward to {APP_URL}")
+
+        return APP_URL, no_update, no_update
 
     if not token:
-        return no_update
+        return no_update, no_update, no_update
         # return [], "No token found. You may need to authorize first."
 
     # Call DRF using the Bearer token
@@ -80,16 +98,29 @@ def initial_view(n):
         if people_resp.status_code == 200:
             # Return raw JSON as a string
             data = people_resp.json()
-            people_res = data.get("results", [])
+            people_res = [
+                {'id':p['id'],
+                 'first_name':p['person']['first_name'] if p['person'] else None,
+                 'last_name':p['person']['last_name'] if p['person'] else None,
+                 'email':p['person']['email'] if p['person'] else None,
+                 'sport':p['sport']['name'] if p['sport'] else None,
+                 'enrollment_status':p['current_enrollment']['enrollment_status'] if p['current_enrollment'] else None
+                 }
+                for p in data.get("results", [])]
+
+            print(people_res)
 
         if user_resp.status_code == 200:
             data = user_resp.json()
             user_res = json.dumps(data,indent=2)
 
-        return people_res, user_res
+        return no_update, people_res, user_res
 
     except Exception as e:
         print(f"Error calling DRF: {str(e)}")
+
+        traceback.print_exc()
+
         # return [], f"Error calling DRF: {str(e)}"
         return no_update
 
